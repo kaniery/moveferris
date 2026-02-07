e87c077a07a1b6c35d40aae3f3bf9feb16673983
kaniery <kaniryu21@gmail.com>
2026-02-07 19:42:32 +0900

    Add leg

--- src/main.rs
+++ src/main.rs
@@ -2,32 +2,56 @@ use std::thread;
 use std::time::Duration;
 
 fn main() {
-    let ferris = r#"                                                  
-                  - .== =+:.=-  :                 
-              .- =++++++++++++++= :+              
-    .       ::-+++++++++++++++++++++:::     :-  . 
- =- -+=     ++++++++++++++++++++++++++-   -++- :+ 
- ++-=++= .=+++++++++++++++++++++++++++++- -+++++- 
-  -++++ ===+++++++++++++++++++++++++++++==:.++-   
-    :++.:+++++++++++- +@#++*- %%++++++++++=+-     
-      .+++++++++++++*@@@%++*@@@@+++++++++++-      
-      ++--**++++++++++++++++++++++++++*++*++=     
-       -+-.*  .----*******#*****+---   :-.=:      
-         :+                             .=.       
-           :.                           : 
-    "#;
+    // 2フレームを用意して、下部の1箇所を切り替えることで足を動かす
+    let ferris_frames: [&str; 2] = [
+        r#"                                                  
+                 .:.:.:.:.:..;.;.                                     
+              .-.:RRRRRRRRRRRRRRRR=::       ..     
+   .        ::-RRRRRRRRRRRRRRRRRRRRR-::  .:R:  . 
+:. ;R.     +RRRRRRRRRRRRRRRRRRRRRRRRR-   -RR-  :R 
+*R-=RR= .=RRRRRRRR R@RRRRR R@RRRRRRRRR-  -RRRRR- 
+  -RRRR ===RRRRRRR1  @#RRR1  @#RRRRRRRR==:.RR-   
+   :RR.:RRRRRRRRR@_%@#RRR@_%@#RRRRRRRRRRR=R-     
+      .RRRRRRRRRRRRR@RRRRRRR@RRRRRRRRRRRRRR-      
+     ++RRRRRRRRRRRRRR@._.@RRRRRRRRRRRRRR.*RR=     
+       -+R.* .RRRRRRRRRRRRRRRRRRRR.   :R.R:       
+         :R                            .=.        
+           :.                          :
 
-    for i in 0..20 {
-        // 1. 画面をクリア（または改行で押し上げる）
+    "#,
+        r#"                                                  
+                 .:.:.:.:.:..;.;.                                     
+              .-.:RRRRRRRRRRRRRRRR=::       ..     
+   .        ::-RRRRRRRRRRRRRRRRRRRRR-::  .:R:  . 
+:. ;R.     +RRRRRRRRRRRRRRRRRRRRRRRRR-   -RR-  :R 
+*R-=RR= .=RRRRRRRR R@RRRRR R@RRRRRRRRR-  -RRRRR- 
+  -RRRR ===RRRRRRR1  @#RRR1  @#RRRRRRRR==:.RR-   
+   :RR.:RRRRRRRRR@_%@#RRR@_%@#RRRRRRRRRRR=R-     
+      .RRRRRRRRRRRRR@RRRRRRR@RRRRRRRRRRRRRR-      
+     ++RRRRRRRRRRRRRR@._.@RRRRRRRRRRRRRR.*RR=     
+       -+R.* .RRRRRRRRRRRRRRRRRRRR.   :R R:       
+     .:R                                 .=.        
+    .:                                      :
+
+    "#,
+    ];
+
+    // フレーム数と移動幅を調整
+    let frames = ferris_frames.len();
+    for i in 0..40 {
+        // 1. 画面をクリア
         print!("{}[2J{}[H", 27 as char, 27 as char);
 
-        // 2. 左側にスペースを入れて位置をずらす
-        let indent = " ".repeat(i);
-        for line in ferris.lines() {
-            println!("{}{}", indent, line);
+        // 2. 左にスペースを入れて位置をずらす（戻りも可能）
+        let indent: String = " ".repeat(i % 20);
+
+        // 今回はフレームを交互に切り替えて足を動かす
+        let frame = ferris_frames[i % frames];
+        for line in frame.lines() {
+            println!("{}\x1b[31m{}\x1b[0m", indent, line);
         }
 
-        // 3. 少し待機（秒間10フレーム程度）
+        // 3. 少し待機（約10FPS）
         thread::sleep(Duration::from_millis(100));
     }
 }
